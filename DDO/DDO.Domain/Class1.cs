57ee99ba8b3592b3fd84bd453d4ccb48d71b9bd4
Vishal Rathore <vishalrathor9211@gmail.com>
2018-09-19 12:55:40 +0530

    Project setup and creation implemented

---
+++ DDO/DDO.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DDO.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ DDO/DDO.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DDO.Persistence
+{
+    public class Class1
+    {
+    }
+}
